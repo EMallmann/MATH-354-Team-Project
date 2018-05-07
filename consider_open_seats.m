aed5019443385a3b79407fa5cf74acf0407033e6
EMallmann <ericmallmann@gmail.com>
2018-05-07 11:28:30 -0500

    Update consider_open_seats.m

--- consider_open_seats.m
+++ consider_open_seats.m
@@ -8,21 +8,21 @@
 %for that student at the same time period instead of this one first.
 
 function [open_students_at_time_slot,open_seats,students] = consider_open_seats(open_seats, time_slot, major_required, num_students)
-    open_students_at_time_slot = 0;
+    open_students_at_time_slot = 0; %initialize open students at 0.
     students = num_students;
     for ii = 1: length(time_slot) %for each time period
         for jj = 1 : length(major_required) %for each major
             
            if time_slot(ii) == major_required(jj) && time_slot(ii) ~= "" && major_required(jj) ~= "" %does the student need this class? && exclude empty strings matching
                if open_seats(ii) >= students %more seats than students
-                    open_students_at_time_slot = open_students_at_time_slot + students; %add number of students avaialable 
-                   open_seats(ii) = open_seats(ii) - students;
-                    students = 0;
+                    open_students_at_time_slot = open_students_at_time_slot + students; %add number of students available 
+                   open_seats(ii) = open_seats(ii) - students; %adjust the number of open seats
+                    students = 0; %number of students open to take classes at this time period zero
                     
                else %more students than seats
                     open_students_at_time_slot = open_students_at_time_slot + open_seats(ii); %add number of seats available
-                   open_seats(ii) = 0;
-                   students = students - open_seats(ii);
+                   open_seats(ii) = 0; %no open seats left
+                   students = students - open_seats(ii); %adjust the number of students to open to take classes at this time period
                   
                     
                end %ends students and seats check
